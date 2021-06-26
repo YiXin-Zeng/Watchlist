c4ac9cacbfc69cdfbfab76595708bc09df318692
YiXin-Zeng <793777312@qq.com>
2021-06-26 16:59:07 +0800

    new file:   .idea/.gitignore 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/watchlist.iml 	new file:   __pycache__/app.cpython-36.pyc         new file:   __pycache__/app.cpython-37.pyc 	new file:   __pycache__/models.cpython-36.pyc 	new file:   app.py 	new file:   models.py 	new file:   static/favicon.ico 	new file:   static/images/avatar.png 	new file:   static/images/totoro.gif 	new file:   static/style.css 	new file:   templates/index.html

---
+++ app.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/6/25 11:17 
+# @Author : yixinzeng
+# @File : app.py
+# @use:
+
+import click
+from flask import Flask, render_template
+from flask import url_for, escape
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+
+
+app = Flask(__name__)
+app.debug = True
+app.config['JSON_AS_ASCII'] = False  # 设置网页支持中文编码
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root074411@127.0.0.1:3306/watchlist?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+manager = Manager(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    username = db.Column((db.String(20)))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+@app.route('/')
+def index():
+    # return render_template('index.html', name=name, movies=movies)
+    return "hello watchlist"
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
+
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
+
+
+# 新建表时新建数据库
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Maple'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/6/25 16:14 
+# @Author : yixinzeng
+# @File : models.py 
+# @use:定义flask的model，一个model对应数据库的一张表
+from app import db
+
+
